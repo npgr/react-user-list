906e4728071ac97933bab20687e4fb5f7afeb23b
ngoncalvesrodrigues <nunopedro.goncalvesrodrigues@soprasteria.com>
2021-03-02 10:55:16 +0100

    improve reducers

--- src/store/auth/auth.reducer.ts
+++ src/store/auth/auth.reducer.ts
@@ -1,7 +1,15 @@
 import { AnyAction } from "redux";
 import AUTH_TYPES from "./auth.types";
 
-const defaultState = {
+interface IAuthState {
+  initialized: boolean;
+  loading: boolean;
+  token: string | null;
+  user: string | null;
+  error: string | null;
+}
+
+const INITIAL_STATE: IAuthState = {
   initialized: false,
   loading: false,
   token: null,
@@ -9,42 +17,48 @@ const defaultState = {
   error: null,
 };
 
-export default (state = defaultState, { type, payload }: AnyAction) => {
-  switch (type) {
-    case AUTH_TYPES.SET_TOKEN:
-      return {
-        ...state,
-        initialized: true,
-        token: payload.token,
-        user: payload.user,
-      };
-    case AUTH_TYPES.LOGIN_PENDING:
-      return {
-        ...state,
-        loading: true,
-        token: null,
-        error: null,
-      };
-    case AUTH_TYPES.LOGIN_FULFILLED:
-      return {
-        ...state,
-        loading: false,
-        token: payload.token,
-        user: payload.user,
-      };
-    case AUTH_TYPES.LOGIN_REJECTED:
-      return {
-        ...state,
-        loading: false,
-        error: payload || "User or password incorrect",
-      };
-    case AUTH_TYPES.LOGOUT:
-      return {
-        ...state,
-        user: null,
-        token: null,
-      };
-    default:
-      return state;
-  }
+const setToken = (state: IAuthState, { payload }: AnyAction) => ({
+  ...state,
+  initialized: true,
+  token: payload.token,
+  user: payload.user,
+});
+
+const loginPending = (state: IAuthState) => ({
+  ...state,
+  loading: true,
+  token: null,
+  error: null,
+});
+
+const loginFulfilled = (state: IAuthState, { payload }: AnyAction) => ({
+  ...state,
+  loading: false,
+  token: payload.token,
+  user: payload.user,
+});
+
+const loginRejected = (state: IAuthState, { payload }: AnyAction) => ({
+  ...state,
+  loading: false,
+  error: payload || "User or password incorrect",
+});
+
+const logout = (state: IAuthState) => ({
+  ...state,
+  user: null,
+  token: null,
+});
+
+const reducerMap = {
+  [AUTH_TYPES.SET_TOKEN]: setToken,
+  [AUTH_TYPES.LOGIN_PENDING]: loginPending,
+  [AUTH_TYPES.LOGIN_FULFILLED]: loginFulfilled,
+  [AUTH_TYPES.LOGIN_REJECTED]: loginRejected,
+  [AUTH_TYPES.LOGOUT]: logout,
 };
+
+const reducer = (state = INITIAL_STATE, action: AnyAction): IAuthState =>
+  reducerMap[action.type] ? reducerMap[action.type](state, action) : state;
+
+export default reducer;
--- src/store/users/users.reducer.ts
+++ src/store/users/users.reducer.ts
@@ -1,7 +1,16 @@
 import { AnyAction } from "redux";
+import { UserDetail, User } from "../../models/user";
 import USER_TYPES from "./users.types";
 
-const defaultState = {
+interface IUserState {
+  loading: boolean;
+  successUpdate: string | null;
+  error: string | null;
+  userList: User[];
+  user: UserDetail | null;
+}
+
+const INITIAL_STATE = {
   loading: false,
   successUpdate: null,
   error: null,
@@ -9,85 +18,99 @@ const defaultState = {
   user: null,
 };
 
-export default (state = defaultState, { type, payload }: AnyAction) => {
-  switch (type) {
-    case USER_TYPES.GET_USER_LIST_PENDING:
-      return {
-        ...state,
-        loading: true,
-      };
-    case USER_TYPES.GET_USER_LIST_FULFILLED:
-      return {
-        ...state,
-        loading: false,
-        userList: payload,
-      };
-    case USER_TYPES.GET_USER_LIST_REJECTED:
-      return {
-        ...state,
-        loading: false,
-        error: payload.error || "Error Loading User List",
-      };
-    case USER_TYPES.GET_USER_PENDING:
-      return {
-        ...state,
-        loading: true,
-      };
-    case USER_TYPES.GET_USER_FULFILLED:
-      return {
-        ...state,
-        loading: false,
-        user: payload,
-      };
-    case USER_TYPES.GET_USER_REJECTED:
-      return {
-        ...state,
-        loading: false,
-        error: payload.error || "Error Loading User",
-      };
-    case USER_TYPES.DELETE_USER_PENDING:
-      return {
-        ...state,
-        loading: true,
-      };
-    case USER_TYPES.DELETE_USER_FULFILLED:
-      return {
-        ...state,
-        loading: false,
-        user: {},
-        successUpdate: "User Successfully deleted",
-      };
-    case USER_TYPES.DELETE_USER_REJECTED:
-      return {
-        ...state,
-        loading: false,
-        error: payload.error || "Error deleting User",
-      };
-    case USER_TYPES.CHANGE_USER_PENDING:
-      return {
-        ...state,
-        loading: true,
-      };
-    case USER_TYPES.CHANGE_USER_FULFILLED:
-      return {
-        ...state,
-        loading: false,
-        user: {},
-        successUpdate: "User Successfully updated",
-      };
-    case USER_TYPES.CHANGE_USER_REJECTED:
-      return {
-        ...state,
-        loading: false,
-        error: payload.error || "Error Changing User",
-      };
-    case USER_TYPES.CLEAR_MESSAGES:
-      return {
-        ...state,
-        successUpdate: null,
-        error: null,
-      };
-    default:
-      return state;
-  }
+const getUserListPending = (state: IUserState) => ({
+  ...state,
+  loading: true,
+});
+
+const getUserListFulfilled = (state: IUserState, { payload }: AnyAction) => ({
+  ...state,
+  loading: false,
+  userList: payload,
+});
+
+const getUserListRejected = (state: IUserState, { payload }: AnyAction) => ({
+  ...state,
+  loading: false,
+  error: payload.error || "Error Loading User List",
+});
+
+const getUserPending = (state: IUserState) => ({
+  ...state,
+  loading: true,
+});
+
+const getUserFulfilled = (state: IUserState, { payload }: AnyAction) => ({
+  ...state,
+  loading: false,
+  user: payload,
+});
+
+const getUserRejected = (state: IUserState, { payload }: AnyAction) => ({
+  ...state,
+  loading: false,
+  error: payload.error || "Error Loading User",
+});
+
+const deleteUserPending = (state: IUserState) => ({
+  ...state,
+  loading: true,
+});
+
+const deleteUserFulfilled = (state: IUserState, { payload }: AnyAction) => ({
+  ...state,
+  loading: false,
+  user: {},
+  successUpdate: "User Successfully deleted",
+});
+
+const deleteUserRejected = (state: IUserState, { payload }: AnyAction) => ({
+  ...state,
+  loading: false,
+  error: payload.error || "Error deleting User",
+});
+
+const changeUserPending = (state: IUserState) => ({
+  ...state,
+  loading: true,
+});
+
+const changeUserFulfilled = (state: IUserState) => ({
+  ...state,
+  loading: false,
+  user: {},
+  successUpdate: "User Successfully updated",
+});
+
+const changeUserRejected = (state: IUserState, { payload }: AnyAction) => ({
+  ...state,
+  loading: false,
+  error: payload.error || "Error Changing User",
+});
+
+const clearMessage = (state: IUserState) => ({
+  ...state,
+  successUpdate: null,
+  error: null,
+});
+
+const reducerMap = {
+  [USER_TYPES.GET_USER_LIST_PENDING]: getUserListPending,
+  [USER_TYPES.GET_USER_LIST_FULFILLED]: getUserListFulfilled,
+  [USER_TYPES.GET_USER_LIST_REJECTED]: getUserListRejected,
+  [USER_TYPES.GET_USER_PENDING]: getUserPending,
+  [USER_TYPES.GET_USER_FULFILLED]: getUserFulfilled,
+  [USER_TYPES.GET_USER_REJECTED]: getUserRejected,
+  [USER_TYPES.CHANGE_USER_PENDING]: changeUserPending,
+  [USER_TYPES.CHANGE_USER_FULFILLED]: changeUserFulfilled,
+  [USER_TYPES.CHANGE_USER_REJECTED]: changeUserRejected,
+  [USER_TYPES.DELETE_USER_PENDING]: deleteUserPending,
+  [USER_TYPES.DELETE_USER_FULFILLED]: deleteUserFulfilled,
+  [USER_TYPES.DELETE_USER_REJECTED]: deleteUserRejected,
+  [USER_TYPES.CLEAR_MESSAGES]: clearMessage,
 };
+
+const reducer = (state = INITIAL_STATE, action: AnyAction): IUserState =>
+  reducerMap[action.type] ? reducerMap[action.type](state, action) : state;
+
+export default reducer;
